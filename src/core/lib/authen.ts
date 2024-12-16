e843b5a3726bc98829614b4f12ff2077ba9bbbb1
Kinkando <TY_Sage@hotmail.com>
2024-12-16 10:04:14 +0700

    chore(deps): bump `firebase` version

--- src/core/lib/authen.ts
+++ src/core/lib/authen.ts
@@ -12,26 +12,31 @@ const auth = getAuth(app);
 auth.languageCode = 'th';
 
 export async function signInWithGoogle() {
+  await auth.authStateReady();
   const provider = new GoogleAuthProvider();
   const response = await signInWithPopup(auth, provider);
   return response?.user;
 }
 
 export async function signInWithEmailPassword(email: string, password: string) {
+  await auth.authStateReady();
   const response = await signInWithEmailAndPassword(auth, email, password);
   return response?.user;
 }
 
 export async function signUpWithEmailPassword(email: string, password: string) {
+  await auth.authStateReady();
   const response = await createUserWithEmailAndPassword(auth, email, password);
   return response?.user;
 }
 
 export async function signout() {
+  await auth.authStateReady();
   await signOut(auth);
 }
 
 export async function getCurrentUser() {
+  await auth.authStateReady();
   await waitForSignin();
   return auth.currentUser!;
 }
